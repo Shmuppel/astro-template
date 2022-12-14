15410726707636b35079bd6499208575a378ac9f
Niels van der Vegt <ns.vandervegt@gmail.com>
2022-12-14 09:01:56 +0100

    :bug: Fix eslint on svelte typescript

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jsx-a11y/recommended',
+    'plugin:astro/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'lastest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte', '.astro'],
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+  overrides: [
+    // Astro
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+    },
+    // Svelte
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  settings: {
+    'svelte3/typescript': require('typescript'),
+  },
+  plugins: ['svelte3', '@typescript-eslint'],
+  ignorePatterns: ['node_modules', '.eslintrc.js'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+}
