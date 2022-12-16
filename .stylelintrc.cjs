f90363ea1e495b9f6e55848144757b2d744d416a
Niels van der Vegt <ns.vandervegt@gmail.com>
2022-12-16 09:23:54 +0100

    :bug: Fix svelte preprocessing of TS and sass for vitest

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,15 +1,13 @@
 module.exports = {
-  "plugins": ["stylelint-order"],
-  "extends": [
-    "stylelint-config-html",
-    "stylelint-config-prettier",
-    "stylelint-config-idiomatic-order"
+  plugins: ['stylelint-order'],
+  extends: [
+    'stylelint-config-html',
+    'stylelint-config-prettier',
+    'stylelint-config-idiomatic-order',
   ],
-  "rules": {
-    "selector-pseudo-class-no-unknown": null,
-    "order/order": [
-      "custom-properties",
-      "declarations"
-    ]
-  }
+  rules: {
+    'selector-pseudo-class-no-unknown': null,
+    'order/order': ['custom-properties', 'declarations'],
+    indentation: 2,
+  },
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
 
 export default getViteConfig({
-  plugins: [svelte({ hot: !process.env.VITEST })],
+  plugins: [
+    svelte({
+      hot: !process.env.VITEST,
+      preprocess: [sveltePreprocess({ typescript: true, sass: true })],
+    }),
+  ],
   test: {
     include: ['tests/{unit,integration}/*.test.{ts,js}'],
     globals: true,
